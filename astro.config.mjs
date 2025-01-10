bdde06eed599171e74d203f968d9543041ea66cd
tan <jolanutan@gmail.com>
2025-01-10 19:58:09 +0800

    Remove deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,8 @@
 import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: "server",
-  adapter: vercel({
-    imageService: true,
-    webAnalytics: {
-      enabled: true,
-    },
-  }),
 });
